2b3dfedb52d42f73a938548240d5b223d87df551
Stepan <sustinov@lincolnlabs.co>
2021-12-30 02:53:09 +0300

    Code refactoring, added the ability to use the bot while downloading a video

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import os
 
 from telegram.ext import Updater, Filters, MessageHandler
 from telegram.ext import CommandHandler
+from telegram.ext.defaults import Defaults
 
 from handlers import start, logger, get_url
 
 TOKEN = os.getenv("TG_TOKEN")
 
-updater = Updater(token=TOKEN)
+updater = Updater(
+    token=TOKEN,
+    defaults=Defaults(run_async=True),
+)
 dispatcher = updater.dispatcher
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Init handlers
     dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.regex(".*"), get_url))
+    dispatcher.add_handler(MessageHandler(Filters.regex('.*'), get_url))
 
-    logger.info(msg="Start poling")
+    logger.info(msg='Start poling')
     updater.start_polling()
